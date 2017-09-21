24d2092ccf66a9d716ca31195474b45492ebedb7
wsc <wsc610322199406081138@outlook.com>
2017-09-21 11:02:37 -0400

    add the LinkList.h file

---
+++ LinkList.h
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+#define int ElemType
+typedef struct LNode{
+  ElemType data;
+  struct LNode *next;
+}LNode,*LinkList;
+
+LinkList CreateList(LinkList *L){
+  LNode *s;
+  int x;
+  L=(linkList)malloc(sizeof(LNode));
+  L->next=NULL;
+  scanf("%d",&x);
+  while(x!=9999){
+    s=(LNode*)malloc(sizeof(LNode));
+    s->data=x;
+    s->next=L->next;
+    L->next=s;
+    scanf("%d",&x);
+  }
+  return L;
+}
